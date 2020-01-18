19ee1dce5a6f5cd92c8917f941b1b81c1e65257d
Mohamed Rashad <thegreatrashad@yahoo.com>
2020-01-18 11:34:54 +0200

    Compact Version

--- beam.py
+++ beam.py
@@ -3,32 +3,26 @@ import numpy as np
 moments = []
 Zs = []
 matrix = []
+Xs = []
+
 L = float(input("Beam Length : "))
 q = float(input("Beam Load q : "))
 EI = float(input("Beam stiffness : "))
 n = float(input("Number of beam parts : "))
-
-h = L/n
+h = L/n #interval
 
 print()
 print("Calculating Moments..")
-for i in range(0,int(n)-1):
-    X = float(input("Enter X" + str(i) + ":"))
-    moments.append((0.5*q*L*X) - (0.5*q*(X**2)))
+Xs = [float(input("Enter X" + str(i) + ":")) for i in range(0,int(n)-1)]
+moments = [(0.5*q*L*X) - (0.5*q*(X**2)) for X in Xs]
 
 print("Calculating Z..")
-for i in range(0,int(n)-1):
-    Zs.append((-h**2 * moments[i]) / EI)
+Zs = [(-h**2 * moments[i]) / EI for i in range(0,int(n)-1)]
 
 print("Building the system of equations..")
-matrix.append([-2,1,0])
-matrix.append([1,-2,1])
-matrix.append([0,1,-2])
-
-print(Zs)
-print("Solution")
-A = np.array(matrix)
+A = np.array([[-2,1,0],[1,-2,1],[0,1,-2]])
 b = np.array(Zs)
+
+print("Solution")
 z = np.linalg.solve(A,b)
 print(z)
-
